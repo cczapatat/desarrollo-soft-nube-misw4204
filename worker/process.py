6d76ea8d3daa4fb4b1928ae5c5985d341ad7eb43
cczapatat <c.zapatat@uniandes.edu.co>
2024-04-06 12:43:27 -0500

    Worker, recorte y ajuste de size de un video, update en bd

--- worker/process.py
+++ worker/process.py
@@ -1,6 +1,7 @@
+import datetime
 from typing import Union
 from worker import run_worker
-from models.task import Task
+from models.task import Task, Status
 from models.declarative_base import session
 from utils.video_util import process_video
 
@@ -15,12 +16,42 @@ def __get_task__(task_id: int) -> Union[Task, None]:
         return None
 
 
-def process(input):
-    print('[Process] New event: {}'.format(str(input)))
-    task_id = input["id"]
+def __update_task__(task: Task, status: Status, path_processed: Union[str, None]) -> Union[Task, None]:
+    try:
+        task.status = status
+        task.path_processed = path_processed
+        task.updated_at = datetime.datetime.now()
+        session.commit()
+
+        return task
+    except Exception as ex:
+        print('[Process][__update_task__] taskId: {}, error {}'.format(str(task.id), str(ex)))
+        return None
+
+
+def __process_and_update__(task: Task) -> Union[Task, None]:
+    result_process_video = process_video(task.path_origin)
+    result_is_correct = True if result_process_video['path_processed'] is not None else False
+
+    status = Status.ERROR if not result_is_correct else Status.PROCESSED
+    path_processed = None if not result_is_correct else result_process_video['path_processed']
+
+    task_updated = __update_task__(task, status, path_processed)
+    print('[Process][__process_and_update__] result: {}'.format(str({
+        "task_id": task.id,
+        "status": status.value,
+        "path_processed": path_processed,
+    })))
+
+    return task_updated
+
+
+def process(input_data):
+    print('[Process] New event: {}'.format(str(input_data)))
+    task_id = input_data["id"]
 
     if task_id is None or type(task_id) is not int:
-        print('[Process] event is incorrect, input: {}'.format(str(input)))
+        print('[Process] event is incorrect, input: {}'.format(str(input_data)))
         return
 
     task = __get_task__(task_id)
@@ -29,13 +60,7 @@ def process(input):
         print('[Process] task {} is not exists'.format(str(task_id)))
         return
 
-    video = process_video(task.path_origin)
-
-    # TODO: Logica de actualizar la BD de acuerdo al resultado
-    if type(video) is dict:
-        print('[Process] mal procesamiento')
-    else:
-        print('[Process] buen procesamiento')
+    __process_and_update__(task)
 
 
 if __name__ == '__main__':
--- worker/utils/video_util.py
+++ worker/utils/video_util.py
@@ -1,4 +1,15 @@
+import os
+from datetime import datetime
+from faker import Faker
 from moviepy.editor import VideoFileClip
+from moviepy.video.fx.resize import resize
+
+# Videos Results configuration
+video_ext = os.environ.get('OUT_EXT_VIDEOS', 'mp4')
+video_max_duration = int(os.environ.get('MAX_DIRECTION_VIDEOS', 20))
+out_result_file = os.environ.get('OUT_FILE_VIDEOS', 'videos/outs')
+
+faker = Faker()
 
 
 def process_video(path):
@@ -9,19 +20,41 @@ def process_video(path):
 
     width_original = video.w
     height_original = video.h
-    print('[VideoUtil] Video size original: W: {}, H: {}'.format(str(width_original), str(height_original)))
+    print('[VideoUtil] Video size original: path: {}, W: {}, H: {}'.format(
+        path,
+        str(width_original),
+        str(height_original)),
+    )
 
     new_video = resize_video(video)
 
     if type(video) is str:
         return {"end": False, "status": "error", "message": new_video}
 
-    return new_video
+    response_save = save_new_video(new_video)
+
+    if type(video) is str:
+        return {"end": False, "status": "error", "message": response_save}
+
+    return {"path_processed": response_save["path_processed"]}
+
+
+def save_new_video(new_video):
+    try:
+        date = datetime.now().strftime("%Y_%m_%d_%H_%M_%S")
+        uuid = faker.unique.iban()
+        path = '{}/{}_{}.{}'.format(out_result_file, date, uuid, video_ext)
+        new_video.write_videofile(path)
+
+        return {"path_processed": path}
+    except Exception as ex:
+        print('[VideoUtil][save_new_video] error {}'.format(str(ex)))
+        return str(ex)
 
 
 def resize_video(video):
     try:
-        new_video = video.resize((1920, 1080))
+        new_video = resize(video, width=1980, height=1080)
 
         width_new = new_video.w
         height_new = new_video.h
@@ -36,8 +69,10 @@ def resize_video(video):
 def get_video_buffer(path: str):
     try:
         video = VideoFileClip(path)
+        duration_current = video.duration
+        max_duration = video_max_duration if duration_current > video_max_duration else duration_current
 
-        return video
+        return video.subclip(0, max_duration)
     except Exception as ex:
         print('[VideoUtil][get_video_buffer] error {}'.format(str(ex)))
         return str(ex)
