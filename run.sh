56aea0eb83481080c21723d7259a958d3563d725
cczapatat <c.zapatat@uniandes.edu.co>
2024-05-02 22:04:32 -0500

    Ajustes de docker-compose to Deploy

--- run.sh
+++ run.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-docker rm worker_nube api_nube
+docker rm worker_nube api_nube api_image
 
-docker rmi desarrollo-soft-nube-misw4204-worker desarrollo-soft-nube-misw4204-api
+docker rmi desarrollo-soft-nube-misw4204-worker desarrollo-soft-nube-misw4204-api desarrollo-soft-nube-misw4204-api_image
 
 docker volume rm desarrollo-soft-nube-misw4204_shared-volume
 
+sudo docker-compose -f docker-compose-base-api.yaml up
 sudo docker-compose -f docker-compose.yaml up
